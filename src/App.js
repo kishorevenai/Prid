cb71a5bd9c8e77c482d090c8b136b0d9816765f4
Venaikishore@5 <kishorevenai@gmail.com>
2023-09-08 12:23:15 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes , Route } from 'react-router-dom'
 import Layout from './components/Layout';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Routes>
        <Route path='/' element={<Layout/>}>
+         <Route index element={<Home/>}></Route>
        </Route>
     </Routes>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import favicon from '../assets/favicon.svg'
 import './components.css'
 import  Navbar from 'react-bootstrap/Navbar'
@@ -7,18 +7,22 @@ import Offcanvas from 'react-bootstrap/Offcanvas'
 import Nav from 'react-bootstrap/Nav';
 
 const Header = () => {
+    const [Hamburger , setHamburger] = useState(false)
+    
    return (
       <div className='header'>
-        <div className='header-inner'>
-          <img className='facvicon' src={favicon}></img>
-          <div className='hamburger'></div>
-          <div className='header-links'>
-             <div>Home</div>             
-             <div>Problem solver</div>
-             <div>Redefining solutions</div>
-             <div>Innovative solutionist</div>
-             <div>Data innovation</div>
-             <div>Contact</div>
+        <div className='header-inner container'>
+          <img className='favicon' src={favicon}></img>
+          <div className='hamburger' onClick={() => setHamburger(prev => !prev)}></div>
+          <div className={Hamburger ? 'navlink active':'navlink'}>
+            <div className='burger-inner'>
+               <div>Home</div>             
+               <div>Problem solver</div>
+               <div>Redefining solutions</div>
+               <div>Innovative solutionist</div>
+               <div>Data innovation</div>
+               <div>Contact</div>
+            </div>
           </div>
         </div>
       </div>
